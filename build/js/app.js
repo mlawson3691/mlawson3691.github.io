7ac3518cb529c8be7ce6e5858d00a4072c10d62f
Mark Lawson <mlawson3691@gmail.com>
2017-01-25 15:12:44 -0800

    add 2d game and add animation to skills

--- build/js/app.js
+++ build/js/app.js
@@ -11,6 +11,14 @@ var Portfolio = require('./../js/portfolio.js').portfolioModule;
 $(document).ready(function() {
 
   $('#skills-page').fadeIn();
+  var pause = 500;
+  $('.skill').each(function(skill) {
+      var self = $(this);
+      setTimeout(function() {
+          self.css('opacity', 1);
+      }, pause);
+      pause += 150;
+  });
 
   // show skills page
   $('#skills').click(function() {
@@ -20,11 +28,22 @@ $(document).ready(function() {
     setTimeout(function() {
       $('#skills-page').fadeIn();
     }, 500);
+    var pause = 500;
+    $('.skill').each(function(skill) {
+        var self = $(this);
+        setTimeout(function() {
+            self.css('opacity', 1);
+        }, pause);
+        pause += 150;
+    });
   });
 
   // match button text to project text to show clicked project
   $('#project-nav > div').click(function() {
     $('#skills-page').fadeOut();
+    $('.skill').each(function(skill) {
+        $(this).css('opacity', 0);
+    });
     var clicked = $(this).children('div').text();
     $('.project').each(function() {
       var current = $(this).children('h2').text();
